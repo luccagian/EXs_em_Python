f3360a3624b4a46c3348e6939beb9748830d9d6b
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:10:52 -0300

    Update ex13_2.py

--- lista02/ex13_2.py
+++ lista02/ex13_2.py
@@ -1,5 +1,11 @@
+"""
+Num determinado Estado, para transferências de veículos, o DETRAN cobra uma taxa
+de 1% para carros fabricados antes de 1990 e uma taxa de 1.5% para os fabricados de
+1990 em diante, a taxa está incidindo sobre o valor de tabela do carro. Escreva um
+programa que leia o ano e o preço do carro e a seguir calcula e imprime imposto a ser pago
 ano = int(input('Digite o ano de seu carro:'))
 valorcarro = int(input('Digite o preço do carro:'))
+"""
 
 if ano < 1990:
     valorcarro =  1.001*valorcarro
