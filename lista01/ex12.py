e97d3391aaf79f606c3505e43b32e0799bdf800c
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:00:40 -0300

    Update ex12.py

--- lista01/ex12.py
+++ lista01/ex12.py
@@ -1,3 +1,14 @@
+"""
+Uma empresa tem para um determinado funcionário uma ficha contendo o nome,
+número de horas trabalhadas e o número de dependentes de um funcionário.
+Considerando que:
+a) A empresa paga 12 reais por hora e 40 reais por dependente.
+b) Sobre o salário são feitos descontos de 8,5% para o INSS e 5% para IR.
+Faça um algoritmo para ler o nome, número de horas trabalhadas e número de
+dependentes de um funcionário. Após a leitura, escreva qual o nome, salário bruto, os
+valores descontados para cada tipo de imposto e finalmente qual o salário líquido do
+funcionário.
+"""
 nome=input('Digite seu nome:')
 num_hora=int(input('Horas trabalhadas:'))
 num_dp=int(input('Numero de depedentes:'))
