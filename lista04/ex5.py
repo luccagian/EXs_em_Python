8013857652f5b605b8301f45932fed1fc785e4b9
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:22:18 -0300

    Update ex5.py

--- lista04/ex5.py
+++ lista04/ex5.py
@@ -1,3 +1,10 @@
+"""
+Escrever um programa que leia n, e mostre todas as potências de 2 menores que n.
+Exemplo:
+Entrada: n = 100
+Saída: 1 2 4 8 16 32 64
+"""
+
 n = int(input('Digite o número aqui:')) 
 nump = 1
 
