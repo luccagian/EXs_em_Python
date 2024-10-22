85470cf9a2dca69885f93aefa575392eb299b5f0
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:22:01 -0300

    Update ex4.py

--- lista04/ex4.py
+++ lista04/ex4.py
@@ -1,3 +1,9 @@
+"""
+Escrever um programa que leia n, e mostre os quadrados menores que n.
+Exemplo:
+Entrada: n = 100
+Saída: 0 1 4 9 16 25 36 49 64 81
+"""
 n = int(input('Digite o número aqui:')) 
 numq = 0
 num = 0
