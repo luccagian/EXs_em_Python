fc2b4bdb72ceffdf2dcae0df7cf772916b883253
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:33:07 -0300

    Update ex4.py

--- lista06/ex4.py
+++ lista06/ex4.py
@@ -1,3 +1,8 @@
+"""
+Faça um programa que leia um número n e imprima se ele é primo ou não. (um número
+primo tem apenas 2 divisores: 1 e ele mesmo! O número 1 não é primo!!!)
+"""
+
 n = int(input())
 a = 0
 
