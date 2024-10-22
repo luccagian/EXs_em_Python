cb40dea9002cf219faf9a8f0a83aa5ab30c1f1f1
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:11:13 -0300

    Update ex3_2.py

--- lista02/ex3_2.py
+++ lista02/ex3_2.py
@@ -1,3 +1,9 @@
+"""
+Leia o salário de um trabalhador e o valor da prestação de um empréstimo. Se a
+prestação for maior que 20% do salário imprima: "Empréstimo não concedido", caso
+contrário imprima: "Empréstimo concedido".
+"""
+
 sal = float(input('seu salario: '))
 prest = float(input('valor da prestação: '))
 
