ad4b69bf71537f1c287e7e49ad0aae099341d82f
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:01:04 -0300

    Update ex13.py

--- lista01/ex13.py
+++ lista01/ex13.py
@@ -1,3 +1,9 @@
+"""
+Faça um algoritmo que, tendo como dados de entrada a distância total (em Km)
+percorrida por um automóvel e a quantidade de combustível em litros consumida para
+percorrê-la, calcule o consumo médio de combustível (km / litro).
+"""
+
 km = float(input('Km rodados:'))
 L = float(input('Litros abastecidos:'))
 
