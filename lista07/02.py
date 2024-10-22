5fa5ce5a4f46e30b1a044006ead0296e8f3f3fcf
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:35:56 -0300

    Update 02.py

--- lista07/02.py
+++ lista07/02.py
@@ -1,3 +1,8 @@
+"""
+Faça uma função e um programa de teste para o cálculo do volume de uma esfera. O
+raio é passado por parâmetro.
+V = 4/3 ∗ π ∗ R^3
+"""
 def vol(r):
     v = 4/3 * (3.14 * r**3)
     return v
