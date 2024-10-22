59e7229f07e6250c18a9c08f6784e34cd8dd9f75
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 21:59:22 -0300

    Update ex10.py

--- lista01/ex10.py
+++ lista01/ex10.py
@@ -1,3 +1,17 @@
+"""
+Escreva um programa para ler as seguintes informações de um funcionário: Nome,
+idade, cargo e o seu salário bruto.
+Considere:
+a) O salário bruto teve um reajuste de 38%.
+b) O funcionário receberá uma gratificação de 20% do salário bruto.
+c) O salário total é descontado em 15%.
+Faça um algoritmo para:
+- Imprimir Nome, idade e cargo.
+- Imprimir o salário bruto.
+- Imprimir o salário líquido.
+
+"""
+
 nome = input("Seu nome: ")
 idade = int(input('Sua idade: '))
 cargo = input('Seu Cargo: ')
@@ -13,5 +27,3 @@ print(f'Nome: {nome}')
 print(f'Idade: {idade}')
 print(f'Cargo: {cargo}')
 print(f'Salário liquido: {sal_liq}')
-
-
