5abf74f552859b5f4d0c14ed702e6860b6a72a74
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:39:30 -0300

    Update 07.py

--- lista07/07.py
+++ lista07/07.py
@@ -1,3 +1,14 @@
+"""
+Escreva uma função que, dada a idade de um nadador, classifique-o em uma das
+seguintes categorias:
+Categoria Idade
+Infantil A    5 a 7
+Infantil B    8 a 10
+Juvenil A     11 a 13
+Juvenil B     14 a 17
+Sênior maiores de 18 anos
+"""
+
 def gp(id):
     if id >= 5 and id <= 7:
         cat = 'infantil A'
