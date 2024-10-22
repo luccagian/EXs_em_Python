6025745b284816c48fd6194f6e51ec6efae33341
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:35:17 -0300

    Update 01.py

--- lista07/01.py
+++ lista07/01.py
@@ -1,3 +1,9 @@
+"""
+Faça uma função para verificar se um número é um quadrado perfeito. Um quadrado
+perfeito é um número inteiro não negativo que pode ser expresso como o quadrado de outro
+número inteiro. Ex: 1, 4, 9...
+"""
+
 def quad(n):
     if n < 0:
         return False
