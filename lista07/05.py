08acad56b0bd55df0ab099143459d0ba29753e6e
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:37:04 -0300

    Update 05.py

--- lista07/05.py
+++ lista07/05.py
@@ -1,3 +1,9 @@
+"""
+Faça uma função que receba um número n, como parâmetro, devolve True se ele é
+primo, e False, caso contrário. (um número primo tem apenas 2 divisores: 1 e ele mesmo! O
+número 1 não é primo!!!)
+"""
+
 n = int(input())
 
 def prim(n):
