763baa443f9c97b239ad048f1bf72936c34cadad
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:43:20 -0300

    Update 05.py

--- lista08/05.py
+++ lista08/05.py
@@ -1,3 +1,13 @@
+"""
+Preencha um dicionário com os dados de 5 alunos
+- Utilize o RA do aluno como chave e uma lista de três notas como valor.
+- Solicite os dados do usuário
+- Percorra o dicionário e exiba a média de cada aluno.
+Exemplo:
+Veja um exemplo da estrutura do dicionário.
+{19230490: [10, 8.0, 7.5], 2002441: [6, 5, 7.5], 2001332: [5,8,9.5]}
+"""
+
 dados_alunos = {}
 
 for i in range(5):
