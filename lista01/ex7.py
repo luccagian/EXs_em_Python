3375a3eeb0b95e73d525cdadfd762789bf0b2d01
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:02:37 -0300

    Update ex7.py

--- lista01/ex7.py
+++ lista01/ex7.py
@@ -1,3 +1,10 @@
+"""
+ Escreva um programa que leia a quantidade de litros de combustível gasto por um
+automóvel, e a distância percorrida. Após isso calcule o valor do rendimento do automóvel
+dados em km/l. Exemplo: A distância percorrida de 472 km utilizando 43 litros de
+combustível tem um rendimento de aproximadamente 10,97 km/l.
+"""
+
 combustivel = float(input("Litros gastos "))
 distancia = float(input("KM rodados "))
 
