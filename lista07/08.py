4dbcfa1bc72fce55397866916bdbb40ec6797f76
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:39:51 -0300

    Update 08.py

--- lista07/08.py
+++ lista07/08.py
@@ -1,3 +1,14 @@
+"""
+Escreva um programa que leia um número inteiro positivo n e em seguida imprima n
+linhas do chamado Triângulo de Floyd. Para n = 6, temos:
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+16 17 18 19 20 21
+"""
+
 def floyd(n):
     num = 0
     for i in range(1, n+1):
