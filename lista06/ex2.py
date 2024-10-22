32b2c07084e7889fb29e31c31b1e4e64ab95c7f1
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:32:24 -0300

    Update ex2.py

--- lista06/ex2.py
+++ lista06/ex2.py
@@ -1,3 +1,7 @@
+"""
+Escreva um programa que gera números entre 1000 e 1999 e mostra aqueles que
+divididos por 11 dão resto 5
+"""
 for i in range(1000,2000):
     if i % 11 == 5:
         print(i)
