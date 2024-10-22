dd6d6f8bf1ffa775ad2e80db71633ede2357192c
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:42:47 -0300

    Update 03.py

--- lista08/03.py
+++ lista08/03.py
@@ -1,3 +1,13 @@
+"""
+Escreva uma função chamada intercala_numeros que recebe como entrada duas listas
+de três elementos e retorna uma lista de seis elementos, com os números intercalados.
+Exemplo: Suponha que as listas de entrada da função sejam:
+[1, 2, 3]
+[4, 5, 6]
+Para estas listas, a função deve retornar:
+[1, 4, 2, 5, 3, 6]
+"""
+
 lista1 = []
 lista2 = []
 lista3 = []
