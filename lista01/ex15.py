32bd31ee27fc2c67f53d66e73648ca4bec4a2862
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:01:45 -0300

    Update ex15.py

--- lista01/ex15.py
+++ lista01/ex15.py
@@ -1,3 +1,9 @@
+"""
+Escreva um programa que leia a largura e altura de uma parede, calcule a área a ser
+pintada e a quantidade de tinta necessário para o serviço, sabendo que cada litro de tinta
+pinta uma área de 2 metros quadrados
+"""
+
 alt=float(input('Altura da parede:'))
 larg=float(input('Largura da parede:'))
 
