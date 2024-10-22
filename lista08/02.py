1f3cca60ab3f68471b160221905f31797e4d97bd
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:42:26 -0300

    Update 02.py

--- lista08/02.py
+++ lista08/02.py
@@ -1,3 +1,16 @@
+"""
+Preencha duas tuplas com 5 números cada, informados pelos usuários. Concatene as duas
+tuplas e exiba a tupla resultante.
+Dica: primeiro crie duas listas, e então, converta as listas em tuplas utilizando a função
+tuple.
+tupla = tuple(lista) # converte lista em tupla
+Exemplo: Suponha que as tuplas contenham os números:
+(3, 1, 5, 3, 5)
+(5, 5, 7, 3, 1)
+Como resultado, o programa deve gerar a tupla:
+(3, 1, 5, 3, 5, 5, 5, 7, 3, 1)
+"""
+
 lista1 = []
 lista = []
 
