a8f6b2640aca1fae8755e73a732641c4d2f1e92f
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:24:33 -0300

    Update 02.py

--- lista05/02.py
+++ lista05/02.py
@@ -1,3 +1,18 @@
+"""
+Em uma eleição presidencial existem quatro candidatos. Os votos são informados
+através de códigos. Os dados utilizados para a contagem dos votos obedecem à seguinte
+codificação:
+- 1,2,3,4 = voto para os respectivos candidatos;
+- 5 = voto nulo;
+- 6 = voto em branco;
+Elabore um programa que leia o código do candidato em um voto. Calcule e escreva:
+- total de votos para cada candidato;
+- total de votos nulos;
+- total de votos em branco;
+Como finalizador do conjunto de votos, tem-se o valor 0.
+
+"""
+
 voto = 7
 cand1 = 0 
 cand2 = 0
