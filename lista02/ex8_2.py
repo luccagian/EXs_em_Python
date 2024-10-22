451e1470de32b2605bcb86357276bcab1409ae6c
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:13:44 -0300

    Update ex8_2.py

--- lista02/ex8_2.py
+++ lista02/ex8_2.py
@@ -1,3 +1,14 @@
+"""
+Escreva um programa que, dada a idade de um nadador, classifique-o em uma das
+seguintes categorias:
+Categoria Idade
+Infantil A    5 a 7
+Infantil B    8 a 10
+Juvenil A     11 a 13
+Juvenil B     14 a 17
+Sênior maiores de 18 anos
+"""
+
 idade = int(input('Digite sua idade: '))
 
 if idade >= 5 and idade <= 7:
