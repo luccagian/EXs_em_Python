2b6b6df6a1a7a07746afc80c9585b5ac130321ba
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:09:02 -0300

    Update ex10_2.py

--- lista02/ex10_2.py
+++ lista02/ex10_2.py
@@ -1,3 +1,9 @@
+"""
+Leia o salário de um trabalhador e o valor da prestação de um empréstimo. Se a
+prestação for maior que 20% do salário imprima: "Empréstimo não concedido", caso
+contrário imprima: "Empréstimo concedido".
+"""
+
 nome = input('Digite se unome completo')
 
 n1 = float(input('Digite a primeira nota'))
