024eecf292f74b1822939d61605184a7d44dc510
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:11:48 -0300

    Update ex5_2.py

--- lista02/ex5_2.py
+++ lista02/ex5_2.py
@@ -1,3 +1,11 @@
+"""
+Leia a idade e o tempo de serviço de um trabalhador e escreva se ele pode ou não se
+aposentar. As condições para aposentadoria são:
+- Ter pelo menos 65 anos,
+- Ou ter trabalhado pelo menos 30 anos,
+- Ou ter pelo menos 60 anos e ter trabalhado pelo menos 25 anos.
+"""
+
 idade = int(input('digite sua idade: '))
 temp = int(input('digite os seus anos trabalhados: '))
 
