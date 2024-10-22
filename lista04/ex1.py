c1883d5b6a04dce90839fd1398208636fbc602e2
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:21:05 -0300

    Update ex1.py

--- lista04/ex1.py
+++ lista04/ex1.py
@@ -1,3 +1,10 @@
+"""
+Escrever um programa que leia n, e mostre os n primeiros números pares.
+Exemplo:
+Entrada: n = 5
+Saída: 0 2 4 6 8
+"""
+
 cont = 1
 n = int(input('Escreva a quantidades de números:'))
 num = 0
