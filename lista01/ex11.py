745017f41777373c0c915fea54c061e23f159711
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:00:18 -0300

    Update ex11.py

--- lista01/ex11.py
+++ lista01/ex11.py
@@ -1,3 +1,15 @@
+"""
+Faça um programa que receba o custo (valor em reais) de um espetáculo teatral e o
+preço do convite (valor em reais) desse espetáculo. Esse programa deve calcular e mostrar,
+nesta ordem:
+a) A quantidade de convites que devem ser vendidos para que pelo menos o custo do
+espetáculo seja alcançado.
+b) A quantidade de convites que devem ser vendidos para que se tenha um lucro de 23%.
+Observe que as quantidades calculadas a serem vendidas devem ser um número inteiro;
+portanto, o resultado da quantidade de convites deve ser arredondado para cima, usando a
+função matemática apropriada.
+"""
+
 from math import ceil
 
 cust = float(input('Custo do show:'))
