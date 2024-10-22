464196c346403bd53540b6bd8ca322d9943dab52
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:09:34 -0300

    Update ex11_2.py

--- lista02/ex11_2.py
+++ lista02/ex11_2.py
@@ -1,3 +1,15 @@
+"""
+Dados três valores X, Y e Z, verificar se eles podem ser os comprimentos dos lados de
+um triângulo e, caso positivo, verificar se é um triângulo equilátero, isósceles e escaleno. Se
+eles não formarem um triângulo, o programa deve imprimir uma mensagem adequada.
+Considere as seguintes propriedades:
+- O comprimento de cada lado em um triângulo é menor que a soma dos outros dois
+lados;
+- Equilátero: tem os comprimentos dos três lados iguais;
+- Isósceles: tem os comprimentos de dois lados iguais;
+- Escaleno: tem os comprimentos dos três lados diferentes.
+"""
+
 print('Verifique se os valores a seguir podem se tornar ou não medidas de um triângulo:')
 x = int(input('Digite um valor:'))
 y = int(input('Digite outro valor:'))
