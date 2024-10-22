3624f7ba10f12d5ba73ba042a103c44f7bfde0cc
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:11:31 -0300

    Update ex4_2.py

--- lista02/ex4_2.py
+++ lista02/ex4_2.py
@@ -1,3 +1,10 @@
+"""
+ Faça um programa que receba a altura e o sexo de uma pessoa e calcule e mostre seu
+peso ideal, utilizando as seguintes fórmulas (onde h corresponde a altura):
+- Homens: (72.7 ∗ h) − 58
+- Mulheres: (62.1 ∗ h) − 44.7
+"""
+
 sexo = input('qual o seu sexo? [M/F]')
 altura = float(input('digite sua altura: '))
 
