7ac7e773ef5257752bce5607edbf07361998acbd
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:43:04 -0300

    Update 04.py

--- lista08/04.py
+++ lista08/04.py
@@ -1,3 +1,12 @@
+"""
+Preencha um dicionário com as informações de 5 produtos
+- Utilize o nome do produto como chave e o preço como valor.
+- Solicite os dados ao usuário
+- Percorra o dicionário e exiba o nome dos produtos com preço superior a R$ 50,00.
+Exemplo:
+Veja um exemplo da estrutura do dicionário.
+{'caneta': 3.0, 'Pen drive': 100.0,'Teclado': 30.0, 'Lápis': 1.5}
+"""
 produtos = {}
 
 for i in range(5):
