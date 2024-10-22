b8bbc7e9806fef763b299ef9cb9b33b2b3dc72d1
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2024-10-21 22:40:20 -0300

    Update 09.py

--- lista07/09.py
+++ lista07/09.py
@@ -1,3 +1,16 @@
+"""
+Crie uma função que receba três valores (obrigatoriamente maiores que zero),
+representando as medidas dos três lados de um triângulo. Elabore funções para:
+(a) Determinar se eles lados formam um triângulo, sabendo que:
+- O comprimento de cada lado de um triângulo é menor do que a soma dos outros
+dois lados.
+(b) Determinar e mostrar o tipo de triângulo, caso as medidas formem um triângulo.
+Sendo que:
+- Chama-se equilátero o triângulo que tem três lados iguais.
+- Denominam-se isósceles o triângulo que tem o comprimento de dois lados iguais.
+- Recebe o nome de escaleno o triângulo que tem os três lados diferentes
+"""
+
 x = int(input('Digite um valor:'))
 y = int(input('Digite outro valor:'))
 z = int(input('Digite um ultimo valor:'))
